30e3d9c9a59835a530a8c2ca1ea4d7afc8433d2e
slavesword <slaven.vulic@gmail.com>
2024-06-30 16:05:55 +0100

    first commit P7

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+function Navbar() {
+  return (
+    <header>
+      <div class="header-background"></div>
+      <div class="header-container">
+        <div class="header-top">
+            Slaven
+          {/* <img src="./img/icon-left-font-monochrome-white.png" alt="Logo" class="logo"> */}
+        </div>
+        <div class="header-bottom"></div>
+        <div class="header-floating">
+          {/* <img src="./img/icon.jpg" alt="Profile" class="profile-image"> */}
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
